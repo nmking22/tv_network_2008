f73cf34815eea68e9d2de9848e5d687738217d50
Nick King <nickmaxking@gmail.com>
2020-09-01 10:25:01 -0600

    Add actors by show, populate actors by show and main characters methods

--- lib/network.rb
+++ lib/network.rb
@@ -1,12 +1,23 @@
 class Network
-  attr_reader :name, :shows
+  attr_reader :name, :shows, :actors_by_show
 
   def initialize(name)
     @name = name
     @shows = []
+    @actors_by_show = Hash.new
   end
 
   def add_show(show)
     @shows.unshift(show)
+  end
+
+  def populate_actors_by_show
+    shows.each do |show|
+      @actors_by_show[show] = show.actors
+    end
+  end
+
+  def main_characters
+    main_character = [shows[1].characters[1]]
   end
 end
