c0f9ecd28519231cb74a09a8d228e060cd81ce3c
Nick King <nickmaxking@gmail.com>
2020-09-01 10:46:28 -0600

    Add tests for shows by actor and prolific actors

--- test/network_test.rb
+++ test/network_test.rb
@@ -65,4 +65,50 @@ class NetworkTest < Minitest::Test
     assert_equal ["David Hasselhoff", "William Daniels"], nbc.actors_by_show[knight_rider]
     assert_equal ["Amy Poehler", "Nick Offerman"], nbc.actors_by_show[parks_and_rec]
   end
+
+  def test_it_can_return_shows_by_actor
+    skip
+    nbc = Network.new("NBC")
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    mitch = Character.new({name: "Mitch Buchannon", actor: "David Hasselhoff", salary: 1_200_000})
+    baywatch = Show.new("Baywatch", "Gregory Bonann", [mitch])
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+
+    nbc.add_show(knight_rider)
+    nbc.add_show(parks_and_rec)
+    nbc.add_show(baywatch)
+
+    expected = {
+      "David Hasselhoff" => [knight_rider, baywatch],
+      "William Daniels" => [knight_rider],
+      "Amy Poehler" => [parks_and_rec],
+      "Nick Offerman" => [parks_and_rec]
+    }
+
+    assert_equal expected, nbc.shows_by_actor
+  end
+
+  def test_it_can_identify_prolific_actors
+    skip
+    nbc = Network.new("NBC")
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    mitch = Character.new({name: "Mitch Buchannon", actor: "David Hasselhoff", salary: 1_200_000})
+    baywatch = Show.new("Baywatch", "Gregory Bonann", [mitch])
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+
+    nbc.add_show(knight_rider)
+    nbc.add_show(parks_and_rec)
+    nbc.add_show(baywatch)
+    nbc.shows_by_actor
+
+    assert_equal ["David Hasselhoff"], nbc.prolific_actors
+  end
 end
