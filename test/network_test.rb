b8f5d1d8e1ac8a4e0437d0f4f66cf88014eb8fee
Nick King <nickmaxking@gmail.com>
2020-09-01 09:51:56 -0600

    Add tests for initialize, attributes and add shows

--- test/network_test.rb
+++ test/network_test.rb
@@ -5,5 +5,54 @@ require './lib/show'
 require './lib/network'
 
 class NetworkTest < Minitest::Test
+  def test_it_exists
+    nbc = Network.new("NBC")
+
+    assert_instance_of Network, nbc
+  end
+
+  def test_it_has_readable_attributes
+    nbc = Network.new("NBC")
+
+    assert_equal "NBC", nbc.name
+    assert_equal [], nbc.shows
+  end
+
+  def test_it_can_add_shows
+    skip
+    nbc = Network.new("NBC")
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+
+    nbc.add_show(knight_rider)
+    nbc.add_show(parks_and_rec)
+
+    assert_equal [parks_and_rec, knight_rider]
+  end
+
+  def test_it_can_identify_main_characters
+    skip
+  end
+
+  def test_it_can_return_actors_by_show
+    skip
+    nbc = Network.new("NBC")
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+
+    nbc.add_show(knight_rider)
+    nbc.add_show(parks_and_rec)
+
+    
+  end
+
 
 end
