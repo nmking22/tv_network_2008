c6f19997777295446698e2e9b26a3018fc0d7949
Nick King <nickmaxking@gmail.com>
2020-09-01 09:27:10 -0600

    Add tests

--- test/show_test.rb
+++ test/show_test.rb
@@ -4,5 +4,48 @@ require './lib/character'
 require '/lib/show'
 
 def ShowTest < Minitest::Test
+  def test_it_exists
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
 
+    assert_instance_of Show, knight_rider
+  end
+
+  def test_it_has_readable_attributes
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_equal "Knight Rider", knight_rider.name
+    assert_equal "Glen Larson", knight_rider.creator
+    assert_equal [michael_knight, kitt], knight_rider.characters
+  end
+
+  def test_it_can_calculate_total_salary
+    skip
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_equal 2600000, knight_rider.total_salary
+  end
+
+  def test_it_can_return_highest_paid_actor
+    skip
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_equal "David Hasselhoff", knight_rider.highest_paid_actor
+  end
+
+  def test_it_can_return_actors
+    skip
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_equal ["David Hasselhoff", "William Daniels"]
+  end
 end
