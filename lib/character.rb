b5f717e43bcc5215a50db1397af636c51cc41b2f
Nick King <nickmaxking@gmail.com>
2020-09-01 09:17:48 -0600

    Initial commit

---
+++ lib/character.rb
@@ -0,0 +1,9 @@
+class Character
+  attr_reader :name, :actor, :salary
+  def initialize(character_hash)
+    @character_hash = character_hash
+    @name = character_hash[:name]
+    @actor = character_hash[:actor]
+    @salary = character_hash[:salary]
+  end
+end
