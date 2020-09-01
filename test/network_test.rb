c40c9f6d3eae26474b0c8fa5096fcae1baa659ec
Nick King <nickmaxking@gmail.com>
2020-09-01 10:25:58 -0600

    Add tests for add show, main characters and actors by show

--- test/network_test.rb
+++ test/network_test.rb
@@ -19,7 +19,6 @@ class NetworkTest < Minitest::Test
   end
 
   def test_it_can_add_shows
-    skip
     nbc = Network.new("NBC")
     kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
     michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
@@ -31,15 +30,10 @@ class NetworkTest < Minitest::Test
     nbc.add_show(knight_rider)
     nbc.add_show(parks_and_rec)
 
-    assert_equal [parks_and_rec, knight_rider]
+    assert_equal [parks_and_rec, knight_rider], nbc.shows
   end
 
   def test_it_can_identify_main_characters
-    skip
-  end
-
-  def test_it_can_return_actors_by_show
-    skip
     nbc = Network.new("NBC")
     kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
     michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
@@ -51,8 +45,24 @@ class NetworkTest < Minitest::Test
     nbc.add_show(knight_rider)
     nbc.add_show(parks_and_rec)
 
-    
+    assert_equal [kitt], nbc.main_characters
   end
 
+  def test_it_can_return_actors_by_show
+    nbc = Network.new("NBC")
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
 
+    nbc.add_show(knight_rider)
+    nbc.add_show(parks_and_rec)
+    nbc.populate_actors_by_show
+
+    assert_equal [parks_and_rec, knight_rider], nbc.actors_by_show.keys
+    assert_equal ["David Hasselhoff", "William Daniels"], nbc.actors_by_show[knight_rider]
+    assert_equal ["Amy Poehler", "Nick Offerman"], nbc.actors_by_show[parks_and_rec]
+  end
 end
