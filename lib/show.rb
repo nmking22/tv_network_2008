d64076de8820b150d9ea65ff1364063bdc5c91d2
Nick King <nickmaxking@gmail.com>
2020-09-01 09:39:49 -0600

    Add mehtods for total salary, highest paid actor, actors

--- lib/show.rb
+++ lib/show.rb
@@ -1,8 +1,28 @@
 class Show
+  attr_reader :name, :creator, :characters
 
   def initialize(name, creator, characters)
     @name = name
     @creator = creator
     @characters = characters
+  end
+
+  def total_salary
+    characters.sum do |character|
+      character.salary
+    end
+  end
+
+  def highest_paid_actor
+    highest_paid = characters.max_by do |character|
+      character.salary
+    end
+    highest_paid.actor
+  end
+
+  def actors
+    actor_list = characters.map do |character|
+      character.actor
+    end
   end
 end
